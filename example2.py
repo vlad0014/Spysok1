30834b23822aa2aded67c914c11e93741832cc99
vlad0014 <155103225+vlad0014@users.noreply.github.com>
2024-02-02 21:29:31 +0200

    branch2

---
+++ example2.py
@@ -0,0 +1,6 @@
+import random
+
+my_list = list(range(1001))
+random.shuffle(my_list)
+
+print(min(my_list))
---
+++ example3.py
@@ -0,0 +1,8 @@
+import random
+
+my_list = list(range(11))
+total_sum = 0
+for num in my_list:
+    total_sum += num
+
+print("Сума всіх елементів у списку:", total_sum)
